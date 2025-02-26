36ee9441933516d6c96f3f32c1697bce72b27a4b
鲁齐辉 <1486876017@qq.com>
2025-02-26 15:33:50 +0800

    feat: 修复窗口最小化后计时重置问题

--- src/main/java/icu/jogeen/stopcoding/StopCodingSettingAction.java
+++ src/main/java/icu/jogeen/stopcoding/StopCodingSettingAction.java
@@ -1,6 +1,7 @@
 package icu.jogeen.stopcoding;
 
 import com.alibaba.fastjson2.JSONObject;
+import com.intellij.ide.AppLifecycleListener;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationDisplayType;
@@ -8,7 +9,6 @@ import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ApplicationActivationListener;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.ui.MessageType;
@@ -16,13 +16,15 @@ import icu.jogeen.stopcoding.data.DataCenter;
 import icu.jogeen.stopcoding.data.SettingData;
 import icu.jogeen.stopcoding.service.TimerService;
 import icu.jogeen.stopcoding.ui.SettingDialog;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Optional;
 
-public class StopCodingSettingAction extends AnAction implements ApplicationComponent, ApplicationActivationListener {
+public class StopCodingSettingAction extends AnAction implements ApplicationComponent, AppLifecycleListener {
 
     private static final Logger LOG = Logger.getInstance(StopCodingSettingAction.class);
 
@@ -33,7 +35,7 @@ public class StopCodingSettingAction extends AnAction implements ApplicationComp
     }
 
     @Override
-    public void applicationActivated(com.intellij.openapi.wm.IdeFrame ideFrame) {
+    public void appFrameCreated(@NotNull List<String> commandLineArgs) {
         NotificationGroup notificationGroup = new NotificationGroup("StopCoding", NotificationDisplayType.BALLOON, true);
 
         SettingData settings = new SettingData();
